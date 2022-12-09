f48e3892a1420135f1ac62a2c3aa27384af78f73
nukeador <nukeador@protonmail.com>
2022-12-09 19:17:20 +0100

    implements web mode auth and resetchat feature

--- chatgpt-telegram.py
+++ chatgpt-telegram.py
@@ -2,22 +2,52 @@ import telebot
 import openai
 import boto3
 import config
+from revChatGPT.revChatGPT import Chatbot
 from libs.stabilityApi import text_to_image
 from pathlib import Path
 
-# Inicializar la biblioteca openai utilizando tu secret key
-openai.api_key = config.openai_api_key
 
-# Crear un cliente de Amazon Polly
-polly_client = boto3.Session(
-    aws_access_key_id=config.aws_access_key_id,
-    aws_secret_access_key=config.aws_secret_access_key,
-    region_name=config.region_name
-).client('polly')
+# Uso de ChatGPT web sin API
+if config.openai_mode == 'web':
+    chatgpt_config = {
+        "email": config.openai_email,
+        "password": config.openai_password
+    }
+    chatbot = Chatbot(chatgpt_config, conversation_id=None)
 
-# Crear una carpeta llamada "audio" en la carpeta en la que se está ejecutando el script
-Path("audio").mkdir(parents=True, exist_ok=True)
-open("audio/response.mp3", "a").close()
+    # Función para generar una respuesta sin usar la API de ChatGPT
+    def generate_response_noapi(prompt):
+        response = chatbot.get_chat_response(prompt, output="text")
+        return response['message']
+# Uso de ChatGPT con API
+else:
+    # Inicializar la biblioteca openai utilizando tu secret key
+    openai.api_key = config.openai_api_key
+
+    # Función para generar una respuesta utilizando la API de ChatGPT
+    def generate_response(prompt):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            temperature=0.5,
+        )
+
+        # Devolver la primera respuesta generada por ChatGPT
+        return response["choices"][0]["text"]
+
+if config.voice_enabled:
+    # Crear un cliente de Amazon Polly
+    polly_client = boto3.Session(
+        aws_access_key_id=config.aws_access_key_id,
+        aws_secret_access_key=config.aws_secret_access_key,
+        region_name=config.region_name
+    ).client('polly')
+
+    # Crear una carpeta llamada "audio" en la carpeta en la que se está ejecutando el script
+    Path("audio").mkdir(parents=True, exist_ok=True)
+    open("audio/response.mp3", "a").close()
 
 # Obtener el token del bot de Telegram y crear un bot
 bot = telebot.TeleBot(token=config.telegram_token)
@@ -26,19 +56,6 @@ bot = telebot.TeleBot(token=config.telegram_token)
 engine_id = config.sd_engine_id
 api_host = config.sd_api_host
 api_key = config.sd_api_key
-
-# Función para generar una respuesta utilizando la API de ChatGPT
-def generate_response(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        temperature=0.5,
-    )
-
-    # Devolver la primera respuesta generada por ChatGPT
-    return response["choices"][0]["text"]
 
 # Función para generar una imagen utilizando la API de stability-sdk
 def generate_image(prompt):
@@ -60,6 +77,10 @@ bot.set_my_commands([
     {
         "command": "/voice",
         "description": "Devuelve la respuesta de ChatGPT, pero adjunta una transcripción como nota de voz generada por Amazon Polly"
+    },
+    {
+        "command": "/resetchat",
+        "description": "Reinicia la conversación a cero"
     }
 ])
 
@@ -79,29 +100,37 @@ def handle_message(message):
             # Enviar la imagen generada al remitente del mensaje
             bot.send_photo(chat_id=message.chat.id, photo=image)
         else:
-            # Generar una respuesta utilizando la API de ChatGPT
-            response = generate_response(text)
+            if text.startswith("/resetchat") and config.openai_mode == "web":
+                chatbot.reset_chat()
+                bot.send_message(chat_id=message.chat.id, text='_Se ha reiniciado la conversación_', parse_mode='MarkdownV2')
+            else:
+                # Generar una respuesta utilizando la API de ChatGPT
+                #response = generate_response(text)
+                if config.openai_mode == 'web':
+                    response = generate_response_noapi(text)
+                else:
+                    response = generate_response(text)
 
-            # Enviar la respuesta generada en texto y en nota de voz al remitente del mensaje
-            bot.send_message(chat_id=message.chat.id, text=response)
+                # Enviar la respuesta generada en texto al remitente del mensaje
+                bot.send_message(chat_id=message.chat.id, text=response)
 
-            # Generar una nota de audio la API de Amazon Polly
-            if text.startswith("/voice"):
-                audio = polly_client.synthesize_speech(
-                    Text=response,
-                    VoiceId='Lucia',
-                    Engine = 'neural',
-                    OutputFormat="mp3"
-                )
+                # Generar una nota de audio la API de Amazon Polly
+                if text.startswith("/voice"):
+                    audio = polly_client.synthesize_speech(
+                        Text=response,
+                        VoiceId='Lucia',
+                        Engine = 'neural',
+                        OutputFormat="mp3"
+                    )
 
-                # Obtener el contenido del archivo de audio generado
-                audio_stream = audio["AudioStream"]
-                # Guardar el contenido del archivo de audio en un archivo local
-                with open("audio/response.mp3", "wb") as file:
-                    file.write(audio_stream.read())
-                #  Enviar nota de voz
-                with open("audio/response.mp3", "rb") as f:
-                    bot.send_voice(chat_id=message.chat.id, voice=f)
+                    # Obtener el contenido del archivo de audio generado
+                    audio_stream = audio["AudioStream"]
+                    # Guardar el contenido del archivo de audio en un archivo local
+                    with open("audio/response.mp3", "wb") as file:
+                        file.write(audio_stream.read())
+                    #  Enviar nota de voz
+                    with open("audio/response.mp3", "rb") as f:
+                        bot.send_voice(chat_id=message.chat.id, voice=f)
     else:
         # Enviar un mensaje al usuario indicando que no está autorizado a recibir respuestas del bot
         bot.send_message(chat_id=message.chat.id, text='[Ah ah ah, ¡no has dicho la palabra mágica](https://media.giphy.com/media/owRSsSHHoVYFa/giphy.gif)', parse_mode='MarkdownV2')
